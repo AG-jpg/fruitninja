e728e120e081d67f50188633668231d8e3d55cb2
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2022-09-07 19:08:43 +0300

    Game Over State

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,8 +5,17 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+   [Header("Elementos del Puntaje")]
    public int puntaje;
    public Text textoPuntaje;
+   public Text puntajeFinal;
+
+   [Header("Elementos Game Over")]
+   public GameObject panelGameOver;
+
+   public void Awake() {
+      panelGameOver.SetActive(false);
+   }
 
    public void AumentarPuntaje()
    {
@@ -16,6 +25,22 @@ public class GameManager : MonoBehaviour
 
    public void TocarBomba()
    {
+      panelGameOver.SetActive(true);
+      textoPuntaje.text = "Score: " + puntaje.ToString();
       Time.timeScale = 0;
+   }
+
+   public void Reiniciar()
+   {
+      puntaje = 0;
+      textoPuntaje.text = "0";
+      Time.timeScale = 1;
+      panelGameOver.SetActive(false);
+      puntajeFinal.text = "0";
+
+      foreach(GameObject g in GameObject.FindGameObjectsWithTag("Interactivo"))
+      {
+         Destroy(g);
+      }
    }
 }
